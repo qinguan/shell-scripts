54beebef36c6b070f4fa246e7f644577afc76576
qinguan <qinguan0619@gmail.com>
2012-12-22 22:47:32 +0800

    script to recode user's behavior via ssh login

---
+++ record_login_user_commands.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+history
+USER_IP=`who -u am i 2>/dev/null | awk '{print $NF}' | sed -e 's/[()]//g'`
+
+if [ $USER_IP == "" ]; then
+	USER_IP=`hostname`
+fi
+
+if [ ! -d /tmp/history ]; then
+	mkdir /tmp/history
+	chmod 777 /tmp/history
+fi
+
+if [ ! -d /tmp/history/${LOGNAME} ]; then
+	mkdir /tmp/history/${LOGNAME}
+	chmod 300 /tmp/history/${LOGNAME}
+fi
+
+DT=`date +%Y%m%d_%H%M%S`
+export HISTFILE="/tmp/history/${LOGNAME}/${USER_IP} history.$DT"
+chmod 600 /tmp/history/${LOGNAME}/*history* 2>/dev/null
