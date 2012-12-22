023afbcd84cb2567b19873f5985e12b92df511f9
qinguan <qinguan0619@gmail.com>
2012-12-22 22:56:04 +0800

    update the history directory

--- record_login_user_commands.sh
+++ record_login_user_commands.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# please put this script under /etc/profile.d
+# logfiles are stored in /tmp/history which can be reset with history_dir
 
+history_dir=/tmp/history
 history
 USER_IP=`who -u am i 2>/dev/null | awk '{print $NF}' | sed -e 's/[()]//g'`
 
@@ -7,16 +10,16 @@ if [ $USER_IP == "" ]; then
 	USER_IP=`hostname`
 fi
 
-if [ ! -d /tmp/history ]; then
-	mkdir /tmp/history
-	chmod 777 /tmp/history
+if [ ! -d $history_dir ]; then
+	mkdir $history_dir
+	chmod 777 $history_dir
 fi
 
-if [ ! -d /tmp/history/${LOGNAME} ]; then
-	mkdir /tmp/history/${LOGNAME}
-	chmod 300 /tmp/history/${LOGNAME}
+if [ ! -d $history_dir/${LOGNAME} ]; then
+	mkdir $history_dir/${LOGNAME}
+	chmod 300 $history_dir/${LOGNAME}
 fi
 
 DT=`date +%Y%m%d_%H%M%S`
-export HISTFILE="/tmp/history/${LOGNAME}/${USER_IP} history.$DT"
-chmod 600 /tmp/history/${LOGNAME}/*history* 2>/dev/null
+export HISTFILE="$history_dir/${LOGNAME}/${USER_IP} history.$DT"
+chmod 600 $history_dir/${LOGNAME}/*history* 2>/dev/null
