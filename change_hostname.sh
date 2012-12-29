dabdbbb13a0af2c2313c1e42602c9801e9bbbe07
root <root@ldap.qiyi.com>
2012-12-29 17:53:07 +0800

    change the hostname

---
+++ change_hostname.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# change the hostname
+
+function show_usage {
+	echo "Usage: $0 hostname"
+	exit 1;
+}
+
+if [ $# -ne 1 ]; then
+	show_usage
+fi
+
+hostname=$1
+
+sed -i /^HOSTNAME=/{s/^/#/g} /etc/sysconfig/network
+echo HOSTNAME=$hostname >> /etc/sysconfig/network
+
+#$IP=`/sbin/ifconfig eth0 | grep "inet addr" | cut -f 2 -d ':' | cut -f 1 -d ' '`
+IP=`/sbin/ifconfig | grep "inet addr" | head -n1 | cut -f 2 -d ':' | cut -f 1 -d ' '`
+echo "$IP $hostname" >> /etc/hosts
+
+hostname $hostname
+service network restart
+
